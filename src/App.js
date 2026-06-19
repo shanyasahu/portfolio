b498ab0f7277720fa65e6b1575090978a8ae2c41
Shanya-Sahu <shanyasahu11@gmail.com>
2026-06-20 00:27:52 +0530

    fixed issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Sidebar from "./components/sidebar";
 import HeroSection from "./components/heroSection";
-import Spinner from "./components/spinner";
 import About from "./components/about";
 import GoodAt from "./components/goodat";
 import Experiences from "./components/experiences";
@@ -13,35 +12,21 @@ import Footer from "./components/footer";
 import Education from "./components/education";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    window.onload = () => {
-      setLoading(false); // Set loading to false once everything on the page has loaded
-    };
-  }, []);
-
   return (
     <div className="App scroll-smooth relative overflow-x-hidden">
-      {loading ? (
-        <Spinner />
-      ) : (
-        <>
-          <Sidebar />
-          <aside>
-            <HeroSection />
-            <About />
-            <GoodAt />
-            <Experiences />
-            <Education />
-            <SkillSec />
-            <ProjectSec />
-            <Contact />
-            <ScrollToTopButtom />
-            <Footer />
-          </aside>
-        </>
-      )}
+      <Sidebar />
+      <aside>
+        <HeroSection />
+        <About />
+        <GoodAt />
+        <Experiences />
+        <Education />
+        <SkillSec />
+        <ProjectSec />
+        <Contact />
+        <ScrollToTopButtom />
+        <Footer />
+      </aside>
     </div>
   );
 }
